15c8c324401a68750f5b25020329c5bbe0e53826
ThanhThuan2k <thuanhuynh.190800@gmail.com>
2021-04-05 19:36:56 +0700

    Cài đặt gói nuget Entity Framework Core

---
+++ DoAnCoSo.Data/DoAnCoSoDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnCoSo.Data
+{
+	public class DoAnCoSoDbContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Data Source=45.119.83.27;Initial Catalog=DoAnCoSo;Persist Security Info=True;User ID=sa;Password=mssql@12345");
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
